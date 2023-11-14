0568f5b50a02af41fad95307c6e50a42d970611f
Sammy Wens <111000998+Sammywens@users.noreply.github.com>
2023-11-14 01:36:11 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-// Script that prints C is fun, Python is cool and JavaScript is amazing.
+// Script that prints C is fun,Python is cool and JavaScript is amazing.
 
-const prog_C = 'C is fun';
-const prog_P = 'Python is cool';
-const prog_J = 'JavaScript is amazing'
-console.log(`${prog_C}\n${prog_P}\n${prog_J}`)
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
