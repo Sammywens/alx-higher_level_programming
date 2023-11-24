c7e97670ad44009321e5ed858f41346836ff3d04
Sammy Wens <111000998+Sammywens@users.noreply.github.com>
2023-11-24 06:48:22 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,12 +12,25 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    # Check if all three command-line arguments are provided
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database_name>".format(argv[0]))
+        exit(1)
+
+    # Extract command-line arguments
+    username, password, database_name = argv[1:4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", user=username, port=3306,
+                         passwd=password, db=database_name)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
 
     for row in rows:
         print(row)
+
+    # Close cursor and connection
+    cur.close()
+    db.close()
